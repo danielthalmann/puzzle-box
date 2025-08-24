955d0b8f2f445d3ce4b9f46cc58e4cd74c6a60e7
Daniel Thalmann <daniel@thalmann.li>
2025-08-24 13:23:26 +0200

    matrix

--- matrix.py
+++ matrix.py
@@ -6,24 +6,46 @@ import time
 # Exemple : matrice 8x8 sur GPIO18 (broche physique 12)
 pixel_pin = board.D18
 num_pixels = 256  # 32x8
-matrix = [[0 for _ in range(8)] for _ in range(32)]
 
+def getMatrix(width, height):
 
+    matrix = [[0 for _ in range(width)] for _ in range(height)]
 
-for y in range(8):
-    i = 0
-    for x in range(255 - y, 0, -16):
-        matrix[i][y] = x
-        i += 2
+    last = (width * height) - 1
 
-for y in range(8):
-    i = 1
-    for x in range(255 -15 + y, 0, -16):
-        matrix[i][y] = x
-        i += 2
+    for x in range(width - 2, -1, -2):
+        for y in range(height - 1, -1, -1):
+            
+            matrix[y][x] = (last - ((width  - x) * height)) + (height - y)
 
+    for x in range(width - 1, -1, -2):
+        for y in range(height - 1, -1, -1):
+            
+            matrix[y][x] = (last - ((width - 1 - x) * height)) - (height - 1 - y)
+
+    return matrix
+            
+            
+matrix = getMatrix(32, 8)
 # 
-print (matrix)
+for y in range(8):
+    print (matrix[y])
+
+
+lettre = {
+    "a":[
+       [0,0,0,0,0],
+       [0,0,0,0,0], 
+       [0,1,1,1,0],
+       [1,0,0,0,1],
+       [0,0,1,0,1],
+       [0,1,0,1,1],
+       [1,0,0,0,1],
+       [0,1,1,1,1],
+    ] 
+}
+
+
 #exit()
 
 
@@ -31,7 +53,7 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False)
 
 
 def xy_to_index(x, y):
-    return matrix[x][y]
+    return matrix[7 - y][x]
 
 def clear(pixels):
     for x in range(256):
@@ -47,12 +69,17 @@ clear(pixels)
 #pixels.show()
 
 
-for x in range(32):
-    for y in range(8):
+for y in range(8):
+    for x in range(32):
         pixels[xy_to_index(x, y)] = (15, 13, 4)
         time.sleep(0.1)
         pixels.show()
-        
+
+
+
+
+
+
 # for x in range(256):
 #     pixels[x] = (15, 13, 4)
 #     time.sleep(0.1)
